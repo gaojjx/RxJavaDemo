e1a7b5c0b44cc147ab99324f0bf96b14223fd160
gaojjx <gaojjx@gmail.com>
2017-10-30 15:20:11 +0800

    Chapter Three

---
+++ app/src/test/java/com/example/com/myapplication/demo/ChapterThreeTest.java
@@ -0,0 +1,16 @@
+package com.example.com.myapplication.demo;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by gaojjx on 30/10/2017.
+ */
+public class ChapterThreeTest {
+    @Test
+    public void practice1() throws Exception {
+        ChapterThree.practice1();
+    }
+
+}
